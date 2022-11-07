7084f962e41ed1235b746c665e0272858120ce3f
Clara Zemborain <zemborainm@udesa.edu.ar>
2022-11-07 12:05:16 -0300

    login

---
+++ myApp/src/components/Login/Login.js
@@ -0,0 +1,48 @@
+import { Text, View, StyleSheet, TouchableOpacity, TextInput } from 'react-native'
+import React, { Component } from 'react'
+import { auth } from '../../firebase/config'
+
+class Login extends Component{
+    constructor(props){
+        super(props)
+            this.state= {
+                email: '',
+                pass: '',
+            }
+        }
+
+    login(email, pass){
+        auth.signInWithEmailAndPassword(email, pass)
+        .then((response) => {
+            this.setState({loggedIn: true});
+        })
+        .catch(error => {
+            this.setState({error: 'Credenciales invalidas.'})
+        })
+    }
+
+    render(){
+        return(
+            <View>
+                <Text>Login</Text>
+                <TextInput
+                keyboardType ='email-address'
+                placeholder = 'email'
+                onChangeText = {text => this.setState({email:text})}
+                value = {this.state.email}
+                />
+
+                <TextInput
+                keyboardType = 'default'
+                placeholder = 'password'
+                onChangeText = {text => this.setState({pass:text})}
+                value = {this.state.pass}
+                />
+
+                <TouchableOpacity onPress = {()=> this.login(this.state.email, this.state.pass)}>
+                    <Text>Login</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+    }
