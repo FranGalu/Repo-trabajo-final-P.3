8db8e31df90e5cd6cc85b5f9caa14324f8625d5c
Clara Zemborain <zemborainm@udesa.edu.ar>
2022-11-14 12:45:12 -0300

    tab navigation

---
+++ myApp/src/navigation/TabNavigation.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import {FontAwesome} from '@expo/vector-icons'
+const Tab = createBottomTabNavigator()
+
+function TabNavigation(){
+    
+}
+
+export default TabNavigation
