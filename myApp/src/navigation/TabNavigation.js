569f0cb6db8c312535918b6b9bb8e2ef06486360
FranGalu <fgalluzzo@udesa.edu.ar>
2022-11-14 18:02:48 -0300

    perfil

--- myApp/src/navigation/TabNavigation.js
+++ myApp/src/navigation/TabNavigation.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import {FontAwesome} from '@expo/vector-icons'
+import perfil from '../screens/Perfil/Perfil'
 const Tab = createBottomTabNavigator()
 
 function TabNavigation(){
---
+++ myApp/src/screens/Perfil/Perfil.js
@@ -0,0 +1,49 @@
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import React, {Component} from 'react'
+import { auth, db } from '../../firebase/config'
+
+class Perfil extends Component {
+
+    //falta pasar las props de navegacion para que cuando haga sign out mande al usuario a la pagina login
+
+    constructor(props){
+        super(props)
+        this.state ={
+            allComments: []
+        }
+    }
+
+    componentDidMount(){
+        db.collections('posts').onSnapshot(docs =>{
+            let comments = []
+            docs.forEach( doc => {
+                comments.push({
+                id: doc.id,
+                data: doc.data()
+                })
+            })
+            this.setState({
+                allComments: comments
+            })
+        }) 
+
+        
+    }
+
+    signOut(){
+        auth.signOut()
+    }
+    render(){
+return (
+    <View>
+      <Text>Este es tu perfil!</Text>
+      <TouchableOpacity onPress={()=> this.signOut()}>
+        <text> Cerrar tu sesión</text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+}
+ 
+
+export default Perfil
