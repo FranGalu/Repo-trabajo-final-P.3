af6635113def933578022c8db42ad6ec268438cb
FranGalu <fgalluzzo@udesa.edu.ar>
2022-11-17 16:53:30 -0300

    perfil amigos base

---
+++ myApp/src/screens/ProfileFriends/ProfileFriends.js
@@ -0,0 +1,39 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+import {db} from '../../firebase/config'
+
+//tengo que entender como usa las props para lograr tener el email del usuario amigo.
+
+export default class ProfileFriends extends Component {
+    constructor(props){
+        super(props)
+        console.log(props)
+        this.state = {
+            mailFriend:props.route.params.email,
+            postsFriend:[]
+        }
+    }
+
+    componentDidMount(){
+        db
+        .collection('posts')
+        .where('owner', '==', this.state.mailFriend)
+        .onSnapshot(docs => {
+            let posts = []
+            docs.forEach(doc => posts.push({
+                id:doc.id,
+                data: doc.data()
+            }))
+            this.setState({
+                postsFriend: posts
+            }, ()=> console.log(this.state.postsFriend))
+        })
+    }
+  render() {
+    return (
+      <View>
+        <Text>ProfileFriends</Text>
+      </View>
+    )
+  }
+}
