d899956832ffcbbf27cb4ff354b89b1d19242731
pilarsauthier <pilarsauthier@gmail.com>
2022-11-21 13:58:23 -0300

    post y home

--- myApp/src/screens/Home/Home.js
+++ myApp/src/screens/Home/Home.js
@@ -1,13 +1,44 @@
 import { View, Text, StyleSheet, Image, FlatList, ActivityIndicator,TouchableOpacity} from 'react-native'
 import React, {Component} from 'react'
+import { db } from '../../firebase/config';
+import Posts from '../Posts/Posts';
 // import {db} from '../../firebase/config'
 
 class Home extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            posts:[],
+        }
+    }
+
+    componentDidMount(){
+        db.collection('posts').orderBy('date', 'desc').onSnapshot(
+            docs => {
+                let posts = [];
+                docs.forEach( oneDoc => {
+                    posts.push({
+                        id: oneDoc.id,
+                        data: oneDoc.data()
+                    })
+                })
+                this.setState({
+                    posts: posts,
+                    loading: false
+                })
+            }
+        )
+    }
 render(){
     return(
-        <View>
-        <Text style={styles.hola}>Hola</Text>
-        
+        <View style={styles.hola}>
+        <Text style={styles.hola}>Home</Text>
+        <FlatList
+        data={this.state.posts}
+        keyExtractor={posts => posts.id}
+        renderItem = { ({item})=> <Posts dataPost={item} />}
+        />
+
         </View>
     )
 }
